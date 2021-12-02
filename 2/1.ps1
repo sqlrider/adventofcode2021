7eb493414ebe2b5cde9939d2a684355148c3df7c
Alex Stuart <68782526+sqlrider@users.noreply.github.com>
2021-12-02 10:13:06 +0000

    Add files via upload

---
+++ 2/1.ps1
@@ -0,0 +1,30 @@
+﻿# https://adventofcode.com/2021/day/2
+# Part 1
+
+$input = Get-Content -Path "$($PSScriptRoot)\input.txt"
+
+$x = 0
+$depth = 0
+
+foreach($row in $input)
+{
+    $direction = $row.Substring(0,1)
+    $magnitude = [int]$row.Substring($row.Length - 1)
+
+    if($direction -eq 'f')
+    {
+        $x += $magnitude
+    }
+    elseif($direction -eq 'd')
+    {
+        $depth += $magnitude
+    }
+    elseif($direction -eq 'u')
+    {
+        $depth -= $magnitude
+    }
+}
+
+$pos = $x * $depth
+
+Write-Output "x = $($x), depth = $($depth), pos = $($pos)"
---
+++ 2/2.ps1
@@ -0,0 +1,32 @@
+﻿# https://adventofcode.com/2021/day/2
+# Part 2
+
+$input = Get-Content -Path "$($PSScriptRoot)\input.txt"
+
+$x = 0
+$depth = 0
+$aim = 0
+
+foreach($row in $input)
+{
+    $direction = $row.Substring(0,1)
+    $magnitude = [int]$row.Substring($row.Length - 1)
+
+    if($direction -eq 'f')
+    {
+        $x += $magnitude
+        $depth += $aim * $magnitude
+    }
+    elseif($direction -eq 'd')
+    {
+        $aim += $magnitude
+    }
+    elseif($direction -eq 'u')
+    {
+        $aim -= $magnitude
+    }
+}
+
+$pos = $x * $depth
+
+Write-Output "x = $($x), depth = $($depth), pos = $($pos)"
